0579e48e68017d710be2a159b8633cf3e81491de
Lucas Bruzzone <lucas@neocoast.com>
2020-05-17 00:06:41 -0300

    Add redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,26 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
+import immutableTransform from 'redux-persist-transform-immutable';
 
 import rootReducers from './reducers/rootReducer';
 import sagas from './sagas/rootSagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const persistConfig = {
+  transforms: [immutableTransform()],
+  key: 'root',
+  storage: AsyncStorage,
+  // Use blacklist to stop persisting the state of specific reducers
+  // blacklist: ['appReducer'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
 const store = createStore(
-  rootReducers,
+  persistedReducer,
   applyMiddleware(sagaMiddleware),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
