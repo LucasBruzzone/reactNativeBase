0f57dc609722fce5ff46fb153ddd2ac6236ef204
Lucas Bruzzone <lucas@neocoast.com>
2020-05-17 00:06:41 -0300

    In progress

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,9 @@ import {
   SafeAreaView,
 } from 'react-native';
 import store from '../redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
 import { Provider } from 'react-redux';
 
 import HomeScreen from './containers/HomeScreen.js/index';
@@ -15,11 +18,19 @@ const App = () => {
     'VirtualizedLists should never be nested',
   ]);
 
+  // const persistor = persistStore(store);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Provider store={store}>
+        {/*
+          <PersistGate loading={null} persistor={persistor}>
+        */}
         <StatusBar barStyle="dark-content" />
         <HomeScreen />
+        {/*
+          </PersistGate>
+        */}
       </Provider>
     </SafeAreaView>
   );
